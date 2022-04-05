d546c62fa0638f360ec1407c9ef3851e7e9937bc
SofiaDavidov <sofi483@gmail.com>
2022-04-05 14:39:34 +0300

    small fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type React from 'react';
 import { st, classes } from './app.st.css';
 import Pokedex from './components/pokedex/Pokedex';
