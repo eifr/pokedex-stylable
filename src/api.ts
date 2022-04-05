839ef710851af5a6b9070497ae92b875e1218b9d
SofiaDavidov <sofi483@gmail.com>
2022-04-05 15:56:16 +0300

    prettier

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
 import type { PokemonInfo, PokemonListItem } from './types';
 
 export const getAllPokemons = async (): Promise<PokemonListItem[]> => {
-    try{
+    try {
         const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1126');
-        return await res.json() as PokemonListItem[];
+        return (await res.json()) as PokemonListItem[];
     } catch {
         return [];
     }
@@ -12,7 +12,7 @@ export const getAllPokemons = async (): Promise<PokemonListItem[]> => {
 export const getPokemonInfo = async (pokemonName: string): Promise<PokemonInfo | null> => {
     try {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        return await res.json() as PokemonInfo;
+        return (await res.json()) as PokemonInfo;
     } catch {
         return null;
     }
