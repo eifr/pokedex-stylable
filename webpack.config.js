845a8dae7509ca0ef02fe2211f9ea09f34b9283f
SofiaDavidov <sofi483@gmail.com>
2022-04-10 10:16:07 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
         fallback: {
             path: require.resolve('@file-services/path'),
             url: require.resolve('url'),
